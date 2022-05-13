a4555216ef5e0f584669a2bdadb091bf50f84f80
RiccardoBeca <riccardobeca@gmail.com>
2022-05-13 14:04:47 +0100

    init

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer>FOOTER</footer>
+</template>
+
+<script>
+export default {
+  name:"FooterComp"
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MainComp.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>MAIN</main>
+</template>
+
+<script>
+export default {
+  name:"MainComp"
+
+}
+</script>
+
+<style>
+
+</style>
